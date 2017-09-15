2836d1a52fe113233a319f3d93767953cb3c95cc
seeacloud <seeacloud@gmail.com>
2017-09-16 00:06:20 +0800

    Initial commit

---
+++ src/pages/index/index.js
@@ -0,0 +1,17 @@
+/**
+ * Created by alex on 9/5/17.
+ */
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+
+// Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  data: {
+    msg: 'geo'
+  },
+  components: {}
+})
